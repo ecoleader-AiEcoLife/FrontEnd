ad26c554a590e4dcf5336858982c36d8a99d2f92
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-28 04:05:15 +0900

    refactor(about): change background and UI for better UX - change background - change UIs - change Contacts UI

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,36 +1,45 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function About() {
-  const [copy, setCopy] = useState<string>("");
+  const [copy, setCopy] = useState<string>('');
 
   const handleCopy = () => {
-    navigator.clipboard.writeText("euAuthnwoo1341@gmail.com");
-    setCopy("이메일이 복사되었습니다.");
-    setTimeout(() => setCopy(""), 2000);
+    navigator.clipboard.writeText('euAuthnwoo1341@gmail.com');
+    setCopy('이메일이 복사되었습니다.');
+    setTimeout(() => setCopy(''), 2000);
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center item-center">
-      <div className="bg-green-200 flex flex-col justify-center items-center">
-        <div className="pb-3">
+    <div className='relative min-h-screen flex flex-col justify-center item-center'>
+      <Image
+        src='/nature.webp'
+        fill
+        priority
+        alt='nature background'
+        className='object-cover'
+      />
+      <div className='z-10 bg-green-200/30 text-[20px] font-semibold py-8 flex flex-col justify-center items-center'>
+        <div className='pb-3'>
           <span>안녕하세요, 경북대학교 컴퓨터학부 </span>
-          <span className="font-bold">Eddy </span>
+          <span className='font-bold text-[25px] text-green-500'>Eddy </span>
           <span>입니다. </span>
         </div>
-        <div className="font-bold"> 프론트엔드 (+풀스택) : 은우</div>{" "}
-        <div className="font-bold"> 백엔드 : 재민</div>
-        <div className="font-bold"> AI : 정주</div>
-        <div className="font-bold"> 카드뉴스 : 준호</div>
+        <div className='w-full bg-slate-700/50 p-5 rounded-xl text-center font-bold'>
+          <h1> 프론트엔드 (+풀스택) : 은우</h1> <h1> 백엔드 : 재민</h1>
+          <h1> AI : 정주</h1>
+          <h1> 카드뉴스 : 준호</h1>
+        </div>
       </div>
 
-      <div className="relative bg-red-500 hover:text-emerald-200 text-center text-white mx-auto rounded-md p-1 mt-8">
+      <div className='relative bg-red-500 hover:text-emerald-200 text-center text-white mx-auto rounded-md p-2 mt-8 font-bold text-[20px] hover:scale-105'>
         <button onClick={handleCopy}>
           <p>Contacts</p>
         </button>
         {copy && (
-          <span className="absolute -translate-x-1/2 top-full mt-2 px-2 py-1 bg-white text-emerald-800 text-sm rounded shadow whitespace-nowrap">
+          <span className='absolute -translate-x-1/2 top-full mt-2 px-2 py-1 bg-white text-emerald-800 text-sm rounded shadow whitespace-nowrap'>
             {copy}
           </span>
         )}
