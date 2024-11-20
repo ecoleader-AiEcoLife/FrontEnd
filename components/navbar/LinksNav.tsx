c132d47a7e3a2f11b247b91db8adfd5c40efea10
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-20 19:11:54 +0900

    chore(navbar): add 'use client' for client component

--- components/navbar/LinksNav.tsx
+++ components/navbar/LinksNav.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -15,9 +16,7 @@ export default function LinksNav() {
   const handleAuthAction = () => {
     if (status === 'loading') {
       <div className='flex justify-center items-center'>
-        <div className='animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-white'>
-          로딩중...
-        </div>
+        <div className='animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-white'>로딩중...</div>
       </div>;
     }
 
@@ -44,24 +43,16 @@ export default function LinksNav() {
     <div className='flex items-center justify-end w-full'>
       <div className='hidden lg:flex items-center space-x-6'>
         <Link href='/about'>
-          <span className='text-white font-semibold hover:text-emerald-200'>
-            About us
-          </span>
+          <span className='text-white font-semibold hover:text-emerald-200'>About us</span>
         </Link>
         <Link href='/board'>
-          <span className='text-white font-semibold hover:text-emerald-200'>
-            정보게시판
-          </span>
+          <span className='text-white font-semibold hover:text-emerald-200'>정보게시판</span>
         </Link>
         <Link href='/chatbot' onClick={handleSessionCheck}>
-          <span className='text-white font-semibold hover:text-emerald-200'>
-            재활용 챗봇
-          </span>
+          <span className='text-white font-semibold hover:text-emerald-200'>재활용 챗봇</span>
         </Link>
         <Link href='/map' onClick={handleSessionCheck}>
-          <span className='text-white font-semibold hover:text-emerald-200'>
-            재활용 지도
-          </span>
+          <span className='text-white font-semibold hover:text-emerald-200'>재활용 지도</span>
         </Link>
       </div>
 
@@ -96,30 +87,16 @@ export default function LinksNav() {
 
       {isMenuOpen && (
         <div className='z-10 lg:hidden absolute top-full right-0 bg-green-800 p-4 mt-2 rounded-lg shadow-lg'>
-          <Link
-            href='/about'
-            className='block py-2 text-white hover:text-emerald-200'
-          >
+          <Link href='/about' className='block py-2 text-white hover:text-emerald-200'>
             About us
           </Link>
-          <Link
-            href='/board'
-            className='block py-2 text-white hover:text-emerald-200'
-          >
+          <Link href='/board' className='block py-2 text-white hover:text-emerald-200'>
             정보게시판
           </Link>
-          <Link
-            href='/chatbot'
-            className='block py-2 text-white hover:text-emerald-200'
-            onClick={handleSessionCheck}
-          >
+          <Link href='/chatbot' className='block py-2 text-white hover:text-emerald-200' onClick={handleSessionCheck}>
             재활용 챗봇
           </Link>
-          <Link
-            href='/map'
-            className='block py-2 text-white hover:text-emerald-200'
-            onClick={handleSessionCheck}
-          >
+          <Link href='/map' className='block py-2 text-white hover:text-emerald-200' onClick={handleSessionCheck}>
             재활용 지도
           </Link>
           {session ? (
