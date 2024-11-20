ac10aaa78b13b4b41955af89032363bda7d03be0
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-20 19:44:10 +0900

    fix: make AuthProvder for client component for session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 import Navbar from '@/components/navbar/Navbar';
+import AuthProvider from '@/components/auth/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <SessionProvider>
+        <AuthProvider>
           <Navbar />
           {children}
-        </SessionProvider>
+        </AuthProvider>
       </body>
     </html>
   );
---
+++ components/auth/AuthProvider.tsx
@@ -0,0 +1,8 @@
+// components/Providers.tsx
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
