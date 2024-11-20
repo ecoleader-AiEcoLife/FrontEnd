0def0c744b0c685dd710f99b8a7b9324e8c5910b
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-20 19:11:00 +0900

    fix: update import path to fix error in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <AuthProvider>
-          <Navbar
+          <Navbar />
           {children}
         </AuthProvider>
       </body>
