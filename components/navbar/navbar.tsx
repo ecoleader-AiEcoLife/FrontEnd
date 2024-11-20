b6d1fbafe6957004302101493bb42ae8a38faf03
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-20 18:39:15 +0900

    refactor(navbar): add Toggle for for responsvie UI

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,20 +1,18 @@
-'use client';
-
-import React from 'react';
-import Logo from '../ui/Logo';
+import Image from 'next/image';
 import LinksNav from './LinksNav';
+import Link from 'next/link';
+import Toggle from './Toggle';
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className='relative w-[100vw] h-[80px] bg-emerald-800 flex justify-center items-center '>
-        <div className='absolute'>
-          <Logo />
-        </div>
-        <div className='w-full flex justify-end pr-[20px]'>
-          <LinksNav />
-        </div>
+    <nav className='w-full h-[80px] bg-emerald-600 flex justify-between items-center '>
+      <Link href='/' className=' w-[100px] flex justify-center hover:scale-105 transform duration-200 ml-[30px]'>
+        <Image src='/logo.webp' width={60} height={60} alt='logo' className=' rounded-full bg-red-400 ' />
+      </Link>
+      <div className='w-full flex justify-center items-center pr-[20px]'>
+        <LinksNav />
       </div>
+      <Toggle />
     </nav>
   );
 }
